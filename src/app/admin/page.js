d15f9feb740bead17fe34c103a7da4479533a74f
HarisShaikh97 <harisahmedsheikh1997@gmail.com>
2024-07-09 03:05:49 +0500

    Admin login page designed...

---
+++ src/app/admin/page.js
@@ -0,0 +1,48 @@
+"use client"
+
+import { useState } from "react"
+
+export default function Login() {
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+
+	const handleSubmit = () => {}
+
+	return (
+		<div className="h-screen w-screen bg-sky-50 flex flex-col gap-10 items-center justify-center">
+			<p className="text-3xl font-semibold">Login</p>
+			<div className="w-[500px] p-10 border border-sky-500 rounded-xl flex flex-col items-center gap-10 bg-white shadow-xl">
+				<div className="w-full flex flex-col gap-2">
+					<p className="font-semibold">Email:</p>
+					<input
+						className="w-full h-12 rounded-xl border px-3"
+						type="email"
+						placeholder="example@example.com"
+						value={email}
+						onChange={(e) => {
+							setEmail(e.target.value)
+						}}
+					/>
+				</div>
+				<div className="w-full flex flex-col gap-2">
+					<p className="font-semibold">Password:</p>
+					<input
+						className="w-full h-12 rounded-xl border px-3"
+						type="password"
+						placeholder="**********"
+						value={password}
+						onChange={(e) => {
+							setPassword(e.target.value)
+						}}
+					/>
+				</div>
+			</div>
+			<button
+				className="bg-sky-500 h-12 w-40 flex items-center justify-center text-white font-semibold rounded-lg shadow-xl"
+				onClick={handleSubmit}
+			>
+				Submit
+			</button>
+		</div>
+	)
+}
