5f9c24359e6a7012ad7d069f35ecf3a3f79da6c8
SHERRY699 <shehryarnasir98@gmail.com>
2024-07-09 01:05:49 +0500

    login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -8,8 +8,8 @@ import Link from "next/link";
 export default function(){
     return(
         <>
-        <section className="w-screen h-screen  flex items-center justify-center bg-[#1d1d27]">
-            <div className="login card w-[500px] h-[700px] bg-white flex flex-col gap-4 items-center justify-start pt-10   shadow-2xl  rounded-lg ">
+        <section className="w-screen h-screen  flex items-center justify-center bg-white">
+            <div className="login card w-[500px] h-[700px] bg-[#1d1d27] flex flex-col gap-4 items-center justify-start pt-10   shadow-2xl  rounded-lg ">
                 <Image src={Logo} width={200} height={200}/>
               <div className="w-full h-full flex flex-col  items-center pt-4 gap-4 justify-start  ">
               <div className="username bg-[#2a293b] flex w-9/12 items-center h-14 rounded-full px-2 gap-2  ">
@@ -21,8 +21,8 @@ export default function(){
                     <input type="password" className="bg-[#2a293b] outline-none text-white"  placeholder="Password"/>
                 </div>
                 <Link href={''} className="text-gray-400">Forget Your Password?</Link>
-                <button>Log In</button>
-                <button>Sign Up</button>
+                <button className="w-[250px] h-[40px] rounded-full bg-[#ca2a5c] text-white font-bold">Log In</button>
+                <button className="text-white font-bold">Sign Up</button>
 
               </div>
             </div>
